139389ed23bc5caee8327ab6112e1655fc81cc50
bragynator <92689909+bragynator@users.noreply.github.com>
2022-05-11 12:58:54 +0300

    notify on timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,13 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+Notify.init({
+  position: 'right-top',
+  rtl: false,
+  timeout: 4000,
+  fontSize: '15px',
+  cssAnimationStyle: 'zoom',
+});
 
 const options = {
   enableTime: true,
@@ -7,12 +15,12 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0].getTime() < Date.now() || selectedDates[0] === undefined) {
+    if (selectedDates[0] === undefined || selectedDates[0].getTime() < Date.now()) {
       startBtn.setAttribute('disabled', '');
 
       selectedDates[0] === undefined
-        ? alert('Please choose a date')
-        : alert('Please choose a date in the future');
+        ? Notify.failure('Please choose a date at all!')
+        : Notify.failure('Please choose a date in the future!');
       return;
     }
 
@@ -26,29 +34,7 @@ let timerId = null;
 const startBtn = document.querySelector('[data-start]');
 const valueFields = document.querySelectorAll('.value');
 
-startBtn.addEventListener('click', () => {
-  startBtn.setAttribute('disabled', '');
-
-  fp.config.clickOpens = false;
-
-  timerId = setInterval(() => {
-    const delta = fp.selectedDates[0].getTime() - Date.now();
-
-    if (delta <= 0) {
-      clearInterval(timerId);
-      fp.config.clickOpens = true;
-      startBtn.removeAttribute('disabled');
-      return;
-    }
-
-    const { days, hours, minutes, seconds } = convertMs(delta);
-
-    valueFields[0].textContent = addLeadingZero(days);
-    valueFields[1].textContent = addLeadingZero(hours);
-    valueFields[2].textContent = addLeadingZero(minutes);
-    valueFields[3].textContent = addLeadingZero(seconds);
-  }, 1000);
-});
+startBtn.addEventListener('click', onStartBtn);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -71,4 +57,25 @@ function convertMs(ms) {
 
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
+}
+
+function onStartBtn(evt) {
+  evt.target.setAttribute('disabled', '');
+
+  timerId = setInterval(() => {
+    const delta = fp.selectedDates[0].getTime() - Date.now();
+
+    if (delta <= 0) {
+      clearInterval(timerId);
+      evt.target.removeAttribute('disabled');
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(delta);
+
+    valueFields[0].textContent = addLeadingZero(days);
+    valueFields[1].textContent = addLeadingZero(hours);
+    valueFields[2].textContent = addLeadingZero(minutes);
+    valueFields[3].textContent = addLeadingZero(seconds);
+  }, 1000);
 }
